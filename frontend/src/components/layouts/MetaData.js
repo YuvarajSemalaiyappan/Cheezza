a5791cc2da0507ae341c1264c8447b09295a1300
YuvarajSemalaiyappan <yuaaraj@gmail.com>
2023-09-04 14:23:52 +0530

    updated

--- frontend/src/components/layouts/MetaData.js
+++ frontend/src/components/layouts/MetaData.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet-async"
 export default function MetaData ({ title}) {
     return (
         <Helmet>
-            <title> {`${title} - A2Z.com`}</title>
+            <title> {`${title} - Cheezza`}</title>
         </Helmet>
 
     )
